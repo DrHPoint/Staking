d7dc3b5c799abe69c0331ecaac8a6e1c99e4a7f9
DrHPoint <hpedlk1007@gmail.com>
2022-02-04 16:36:31 +0700

    Edit MyToken: Ownable => AccessControl

--- contracts/MyToken.sol
+++ contracts/MyToken.sol
@@ -2,14 +2,32 @@
 pragma solidity ^0.8.1;
 
 import "hardhat/console.sol";
-//import "@uniswap/v2-core/contracts/UniswapV2ERC20.sol";
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
-import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/access/AccessControl.sol";
 
-contract MyToken is ERC20, Ownable {
-    constructor() ERC20("MyToken", "MTK") {}
+contract MyToken is ERC20, AccessControl {
+    bytes32 public constant MINTER_ROLE = keccak256("MINTER_ROLE");
+    bytes32 public constant BURNER_ROLE = keccak256("BURNER_ROLE");
 
-    function mint(address to, uint256 amount) public onlyOwner {
+    constructor() ERC20("MyToken", "MTK") {
+        _grantRole(DEFAULT_ADMIN_ROLE, msg.sender);
+        _grantRole(MINTER_ROLE, msg.sender);
+        _grantRole(BURNER_ROLE, msg.sender);
+    }
+
+    function mint(address to, uint256 amount) public onlyRole(MINTER_ROLE) {
         _mint(to, amount);
+    }
+
+    function burn(address from, uint256 amount) public onlyRole(BURNER_ROLE) {
+        _burn(from, amount);
+    }
+
+    function setMinterRole(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        _grantRole(MINTER_ROLE, account);
+    }
+    
+    function setBurnerRole(address account) external onlyRole(DEFAULT_ADMIN_ROLE) {
+        _grantRole(BURNER_ROLE, account);
     }
 }
